8fe8aebeaf07365e1e8349d675aa0db3842dd4bd
namidad <salamonoliwier@gmail.com>
2019-04-10 14:31:44 +0200

    Updates

---
+++ precache-manifest.832cef0e527d65f4dd8bfdc8179f7520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sudoku/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2376657f656b447c3d4b",
+    "url": "/sudoku/static/js/runtime~main.e7756cf6.js"
+  },
+  {
+    "revision": "0e026865dd0b462f25b4",
+    "url": "/sudoku/static/js/main.0f31e18c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/sudoku/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0e026865dd0b462f25b4",
+    "url": "/sudoku/static/css/main.20a03683.chunk.css"
+  },
+  {
+    "revision": "f1eb14a6a6620dda37cc256ca9570d84",
+    "url": "/sudoku/index.html"
+  }
+];
